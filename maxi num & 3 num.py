9dc6a8aae06c50c3ab999b072a56c895503ccfd1
balaji k <balajikbca25@rvu.edu.in>
2026-02-05 10:19:31 +0530

    Add files via upload

---
+++ maxi num & 3 num.py
@@ -0,0 +1,10 @@
+#1RUA25BCA0012 BALAJI K
+a=int(input("ENTER AN FIRST NUMBER:"))
+b=int(input("ENTER AN SECOND NUMBER:"))
+c=int(input("ENTER AN THIRD NUMBER:"))
+if a>b and a>c:
+    print("A is greater")
+elif b>a and b>c:
+    print("B is greater")
+else:
+    print("c is greater")
